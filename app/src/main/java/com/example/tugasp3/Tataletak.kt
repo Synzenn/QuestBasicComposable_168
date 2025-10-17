7a48456074d5d41bb3bc9ac74c2ce12c05749c0d
Synzenn <velriel42@gmail.com>
2025-10-17 10:36:04 +0700

    Memodifikasi letak row, dan menambahkan texk komponen

--- app/src/main/java/com/example/tugasp3/Tataletak.kt
+++ app/src/main/java/com/example/tugasp3/Tataletak.kt
@@ -35,7 +35,13 @@ fun TataLetakColumn(modifier: Modifier) {
 
 @Composable
 fun TataLetakRow(modifier: Modifier) {
-
+    Row(modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
 }
 
 
