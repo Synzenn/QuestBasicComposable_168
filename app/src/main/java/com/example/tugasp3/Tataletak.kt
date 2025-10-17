5f968f11a396ca432c4f3d7056fc6e979097e888
Synzenn <velriel42@gmail.com>
2025-10-17 10:55:42 +0700

    memodifikati tata letak dan memasukkan coloumn 1

--- app/src/main/java/com/example/tugasp3/Tataletak.kt
+++ app/src/main/java/com/example/tugasp3/Tataletak.kt
@@ -81,7 +81,15 @@ fun TataLetakColumnRow(modifier: Modifier) {
 }
 @Composable
 fun TataletakRowColumn(modifier: Modifier){
+    Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly){
+        Column() {
+            Text(text = "Komponen1Kolom1")
+            Text(text = "Komponen2Kolom1")
+            Text(text = "Komponen3Kolom1")
+        }
 
+
+    }
 }
 
 
