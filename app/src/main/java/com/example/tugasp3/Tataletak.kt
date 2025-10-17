eb77596ad4c7bdf0cb6035f83aa03511ec1ab76d
Synzenn <velriel42@gmail.com>
2025-10-17 11:11:56 +0700

    membuat box, dan mengedit image dan text

--- app/src/main/java/com/example/tugasp3/Tataletak.kt
+++ app/src/main/java/com/example/tugasp3/Tataletak.kt
@@ -128,6 +128,27 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
             }
         }
 
+        Spacer(modifier = Modifier.height(height = 10.dp))
 
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(height = 300.dp)
+                .background(color = Color.Cyan),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(painter = gambar,
+                contentDescription = null,
+                contentScale = ContentScale.Fit)
+
+            Text(text = "Kelsya",
+                fontSize = 50.sp,
+                color = Color.Red,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Cursive,
+                modifier = Modifier.align(
+                    alignment = Alignment.Center))
+        }
+    }
 }
 
