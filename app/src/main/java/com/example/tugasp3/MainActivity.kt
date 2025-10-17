ef4ecf306dd34aa2155341eaa052aaa7b87bdd84
Synzenn <velriel42@gmail.com>
2025-10-17 11:12:10 +0700

    menambahkan foto

--- app/src/main/java/com/example/tugasp3/MainActivity.kt
+++ app/src/main/java/com/example/tugasp3/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TugasP3Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    TataletakBoxColumnRow(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
